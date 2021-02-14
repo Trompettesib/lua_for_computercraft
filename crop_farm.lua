8a89b90ddd1ef3b555d88824e0ee10151c7794e4
Trompettesib <60519602+Trompettesib@users.noreply.github.com>
2021-02-14 20:31:35 +0100

    [ADD] first version who doesn 't work
    first things, very very first

---
+++ crop_farm.lua
@@ -0,0 +1,14 @@
+local bloc = {}
+local item = {}
+local bool = false
+bool, bloc = turtle.inspect()
+
+function selectItem (name)
+    turtle.select(1)
+    turtle.
+end
+
+function harvest ()
+    turtle.digDown()
+    turtle.placeDown()
+end
