0ac66ced9838fe5dbc000faceaa914592837d1b6
Trompettesib <60519602+Trompettesib@users.noreply.github.com>
2021-02-27 01:43:47 +0100

    [ADD] add something to select the good item to plant

--- crop_farm.lua
+++ crop_farm.lua
@@ -4,8 +4,14 @@ local bool = false
 bool, bloc = turtle.inspect()
 
 function selectItem (name)
-    turtle.select(1)
-    turtle.
+    local i = 1
+    turtle.select(i)
+    item = turtle.getItemDetail()
+    while (item.name ~= "wheat-seed" and item.name ~= "beetroot-seed" and item.name ~= "potato" and item.name ~= "carrot") do
+        i = i + 1
+        turtle.select(i)
+        item = turtle.getItemDetail()
+    end
 end
 
 function harvest ()
